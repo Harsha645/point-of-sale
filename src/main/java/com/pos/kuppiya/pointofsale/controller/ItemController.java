bd9108c52188f14a133d1e08b25d817b8d2e342b
Harsha645 <harshapramod19980124@gmail.com>
2023-04-04 12:22:26 +0530

    ItemController

---
+++ src/main/java/com/pos/kuppiya/pointofsale/controller/ItemController.java
@@ -0,0 +1,11 @@
+package com.pos.kuppiya.pointofsale.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/item")
+@CrossOrigin
+public class ItemController {
+}
