1cba80d803e1ad28e15c62d740d5734ef426f898
Harsha645 <harshapramod19980124@gmail.com>
2023-04-04 12:23:46 +0530

    Item DTO

---
+++ src/main/java/com/pos/kuppiya/pointofsale/dto/ItermDTO.java
@@ -0,0 +1,19 @@
+package com.pos.kuppiya.pointofsale.dto;
+
+import com.pos.kuppiya.pointofsale.entity.enums.MeasuringUnitType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItermDTO {
+    private int itemId;
+    private String itemName;
+    private MeasuringUnitType measuringUnit;
+    private double balanceQty;
+    private double supplierPrice;
+    private double sellingPrice;
+    private boolean activeState;
+}
